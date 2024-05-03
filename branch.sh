b153d35dfea14239220ab54a7d2cc7c5b7a5b463
Jeff Shillitto <jeff@shotstack.io>
2024-05-04 09:59:20 +1000

    Remove helper scripts

--- branch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the version number from the package.json file
-VERSION=$(grep '"version"' package.json | head -1 | awk -F '"' '{print $4}')
-
-# Checkout a new branch with the version number
-git checkout -b "v$VERSION"
